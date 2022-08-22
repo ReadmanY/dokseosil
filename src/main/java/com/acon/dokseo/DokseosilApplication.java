ff7a33655e04eddba528ed0f685f3c0f27c81eb4
ReadmanY <kuretsan@gmail.com>
2022-08-22 14:12:45 +0900

    first setting

---
+++ src/main/java/com/acon/dokseo/DokseosilApplication.java
@@ -0,0 +1,13 @@
+package com.acon.dokseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokseosilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DokseosilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acon/dokseo/DokseosilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acon.dokseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DokseosilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
